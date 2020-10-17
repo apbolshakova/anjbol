b327dd4bab69b3b7875a835cd7fd235d81dc46eb
Анжела Большакова <julody@mail.ru>
2020-10-17 14:50:53 +0300

    Реализовано меню в мобильной версии

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 function portfolioTabsInit() {
-    var tabLinks = document.querySelectorAll(".b-categories-container__item");
-    var tabContent = document.querySelectorAll(".b-projects-wrapper__projects-container");
+    let tabLinks = document.querySelectorAll(".b-categories-container__item");
+    let tabContent = document.querySelectorAll(".b-projects-wrapper__projects-container");
 
     tabLinks.forEach(function (el) {
         el.addEventListener("click", openTabs);
     });
 
-
-    function openTabs(el) {
-        var btnTarget = el.currentTarget;
-
+    function openTabs() {
         tabContent.forEach(function (el) {
             if (el.classList.contains("active")) {
                 el.classList.remove("active");
@@ -30,4 +27,35 @@ function portfolioTabsInit() {
     }
 }
 
+function mobileMenuInit() {
+    let toggleBtn = document.querySelector(".b-header-wrapper__icon_mobile-menu");
+    let menuWrapper = document.querySelector(".b-main-container__nav-wrapper");
+    let menuLinks = document.querySelectorAll(".b-nav-menu-item__link");
+
+    toggleBtn.addEventListener("click", toggleMenu);
+
+    menuLinks.forEach(function (el) {
+        el.addEventListener("click", closeMenuOnLinkClick);
+    });
+
+    function toggleMenu(e) {
+        if (menuWrapper.classList.contains("active")) {
+            e.target.innerHTML = "menu";
+            menuWrapper.classList.remove("active");
+
+        }
+        else {
+            e.target.innerHTML = "close";
+            menuWrapper.classList.add("active");
+        }
+        
+    }
+
+    function closeMenuOnLinkClick() {
+        toggleBtn.innerHTML = "menu";
+        menuWrapper.classList.remove("active");
+    }
+}
+
 portfolioTabsInit();
+mobileMenuInit();
